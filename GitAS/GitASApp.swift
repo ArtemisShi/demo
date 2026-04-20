743758bf7e77a1672e44af089a8226d0c681bdc3
BT_C09_15 <mobileappdev@bths.edu>
2026-04-20 13:54:53 -0400

    Initial Commit

---
+++ GitAS/GitASApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitASApp.swift
+//  GitAS
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GitASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
